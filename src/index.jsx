95e472c4e692e3392a01f298e6903b93abcac1b1
Lance Cummings <lance@glance.ca>
2020-01-31 14:59:06 -0800

    First commit

---
+++ src/index.jsx
@@ -0,0 +1,1 @@
+export { useCssBreakpoints } from './useCssBreakpoints'
---
+++ src/useCssBreakpoints.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useRef } from 'react'
+import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect'
+
+const isBrowser = typeof window !== `undefined`
+
+function getBreakpoint() {
+  if (!isBrowser) return { '' }
+  return window.getComputedStyle(document.body, ':before').content.replace(/\"/g, '')
+}
+
+export function useCssBreakpoints(effect, deps, wait) {
+  const ref = useRef(getBreakpoint())
+  let throttleTimeout = null
+
+  const callBack = () => {
+    const breakpoint = getBreakpoint()
+    effect({ prevBreakpoint: ref.current, breakpoint })
+    ref.current = breakpoint
+    throttleTimeout = null
+  }
+
+  useIsomorphicLayoutEffect(() => {
+    if (!isBrowser) {
+      return
+    }
+
+    const handleResize = function() {
+      if (wait) {
+        if (throttleTimeout === null) {
+          throttleTimeout = setTimeout(callBack, wait)
+        }
+      } else {
+        callBack()
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    if (wait) {
+      setTimeout(callBack, wait);
+    }
+ 
+    return () => window.removeEventListener('resize', handleResize)
+  }, deps)
+}
+
+useCssBreakpoints.defaultProps = {
+  deps: [],
+  wait: 10,
+}
